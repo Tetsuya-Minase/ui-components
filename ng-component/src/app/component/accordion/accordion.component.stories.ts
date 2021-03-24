c69df2626b94b4b257eec2d94f166dcdf9d9ed2f
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-03-24 23:15:58 +0900

    feat: accordion base

---
+++ ng-component/src/app/component/accordion/accordion.component.stories.ts
@@ -0,0 +1,11 @@
+import { AccordionComponent } from './accordion.component';
+export default {
+  title: 'accordion component',
+};
+
+export const AccordionDefault = () => ({
+  component: AccordionComponent,
+  props: {
+  }
+});
+
