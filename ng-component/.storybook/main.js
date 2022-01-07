d14d0a12d19df02fbc783817a8e65944a7ee7a6a
Tetsuya-Minase <tetsuya.minasen@gmail.com>
2022-01-07 22:58:19 +0900

    build: update storybook

--- ng-component/.storybook/main.js
+++ ng-component/.storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
-  stories: ['../src/**/*.stories.ts'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes'],
+  stories: ['../src/**/*.stories.@(ts|mdx)'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes', '@storybook/addon-docs', '@storybook/addon-controls'],
+  core: {
+    builder: "webpack5"
+  }
 };
